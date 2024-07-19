cd27016b14f30a247d0d323692d643efebf854a2
AdarshPandeyDevelop <adarsh.pandey11018@gmail.com>
2024-07-19 23:56:33 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn1 = document.querySelector("#btn1")
+btn1.addEventListener("click", ()=>[
+    window.location.href = "btn1.html"
+])
+
+let btn2 = document.querySelector("#btn2")
+btn2.addEventListener("click", ()=>{
+    window.location.href = "btn2.html"
+})
+
+
+
